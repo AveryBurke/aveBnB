b2d0c98d3956b67da551582081683138424454a2
Avery Burke <DrFroid01@gmail.com>
2024-02-28 15:33:56 -0800

    moves navbar into hydration boundry

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,9 +28,9 @@ export default async function RootLayout({ children }: { children: React.ReactNo
 					<LoginModal />
 					<RegistarModal />
 					<RentModal />
+					<Navbar user={currentUSer} />
 				</HydrationBoundary>
-				<Navbar user={currentUSer} />
-				{children}
+				<div className="pb-20 pt-28">{children}</div>
 			</body>
 		</html>
 	);
